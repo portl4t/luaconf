c68e2024e9b193071853ef03f7e1a08730a67e76
portl4t <portl4t.cn@gmail.com>
2014-05-08 01:34:52 +0800

    refine function

--- src/luaconf.c
+++ src/luaconf.c
@@ -2,6 +2,9 @@
 #include "luaconf.h"
 
 
+static luaconf_elt * luaconf_get(lua_State *L, const char *start, const char *end);
+
+
 luaconf_inst *
 luaconf_init(const char *conf)
 {
@@ -61,17 +64,215 @@ luaconf_destroy(luaconf_inst *inst)
     free(inst);
 }
 
-
-luaconf_elt *
-luaconf_getElts(luaconf_inst *inst, luaconf_elt *elt, const char *path, size_t *n)
+void
+luaconf_freeElt(luaconf_elt *elt)
 {
-    *n = 0;
+    int         top;
+    lua_State   *L;
 
-    if (elt) {
-        if (elt->type != LUACONF_TYPE_TABLE)
-            return NULL;
+    L = elt->L;
+    top = lua_gettop(L);
+
+    if (top == elt->pos) {
+        lua_pop(L, -1);
+
+        top  = lua_gettop(L);
+        while (top > 0 && lua_isnil(L, -1)) {
+            lua_pop(L, -1);
+        }
 
     } else {
+        lua_pushnil(L);
+        lua_replace(L, elt->pos);
     }
+
+    free(elt);
+}
+
+luaconf_Number
+luaconf_getNumber(luaconf_elt *elt)
+{
+    luaconf_Number  num;
+
+    LUACONF_ASSERT(elt && elt->type == LUACONF_TYPE_NUMBER);
+
+    num = lua_tonumber(elt->L, elt->pos);
+
+    return num;
+}
+
+size_t
+luaconf_getStrLen(luaconf_elt *elt)
+{
+    LUACONF_ASSERT(elt && elt->type == LUACONF_TYPE_STRING);
+
+    return lua_objlen(elt->L, elt->pos);
+}
+
+char *
+luaconf_getStr(luaconf_elt *elt, char *buf, size_t size, size_t *len)
+{
+    const char  *var;
+    size_t      var_len;
+
+    LUACONF_ASSERT(elt && elt->type == LUACONF_TYPE_STRING);
+
+    var = lua_tolstring(elt->L, elt->pos, &var_len);
+
+    if (size >= var_len) {
+        *len = var_len;
+        memcpy(buf, var, var_len);
+        return buf;
+    }
+
+    return NULL;
+}
+
+luaconf_elt *
+luaconf_getElt(luaconf_inst *inst, const char *path, size_t path_len)
+{
+    int             n;
+    lua_State       *L;
+    luaconf_elt     *new_elt;
+    const char      *begin, *end, *ptr;
+
+    L = inst->L;
+    n = lua_gettop(L);
+    new_elt = NULL;
+
+    lua_pushvalue(L, LUA_GLOBALSINDEX);     // push global table
+
+    if (path && path_len > 0) {
+        begin = path;
+        ptr = path;
+        end = ptr + path_len;
+
+        while (begin < end && !isalpha(*begin))
+            begin++;
+
+        while (begin < end && !isalpha(*(end-1)))
+            end--;
+
+        if (begin >= end)
+            goto nbrch;
+
+        return luaconf_get(L, start, end);
+    }
+
+nbrch:
+
+    new_elt = (luaconf_elt*)malloc(sizeof(luaconf_elt));
+    if (!new_elt) {
+        lua_pop(L, 1);      // pop global table
+        return NULL;
+    }
+
+    new_elt->pos = n - 1;
+    new_elt->type = LUACONF_TYPE_TABLE;     // global table
+    new_elt->vname[0] = 0;
+
+    return new_elt;
+}
+
+luaconf_elt *
+luaconf_getEltElt(luaconf_elt *elt, const char *path, size_t path_len)
+{
+    int         n;
+    const char  *begin, *end, *ptr;
+
+    n = lua_gettop(L);
+
+    lua_pushvalue(elt->L, elt->pos);
+
+    if (path && path_len > 0) {
+        begin = path;
+        ptr = path;
+        end = ptr + path_len;
+
+        while (begin < end && !isalpha(*begin))
+            begin++;
+
+        while (begin < end && !isalpha(*(end-1)))
+            end--;
+
+        if (begin >= end)
+            goto nbrch;
+
+        return luaconf_get(L, start, end);
+    }
+
+nbrch:
+
+    new_elt = (luaconf_elt*)malloc(sizeof(luaconf_elt));
+    if (!new_elt) {
+        lua_pop(L, 1);      // pop table
+        return NULL;
+    }
+
+    new_elt->pos = n - 1;
+    new_elt->type = LUACONF_TYPE_TABLE;     // global table
+    new_elt->vname[0] = 0;
+
+    return new_elt;
+}
+
+static luaconf_elt *
+luaconf_get(lua_State *L, const char *begin, const char *end)
+{
+    int             n;
+    const char      *ptr, *start;
+    luaconf_elt     *new_elt;
+
+    n = lua_gettop(L);
+    new_elt = NULL;
+
+    start = begin;
+    ptr = begin;
+
+    do {
+        ptr++;
+
+        if (*ptr == '.') {
+            if (ptr > start) {
+                lua_pushlstring(L, start, ptr-start);
+                lua_gettable(L, -2);
+                if (!lua_istable(L, -1)) {        // not table, has no member at all
+                    goto failed;
+                }
+            }
+
+            ptr++;
+            start = ptr;
+        }
+    } while (ptr < end);
+
+    if (ptr > start) {
+        lua_pushlstring(L, start, ptr-start);
+        lua_gettable(L, -2);
+        if (lua_isnil(L, -1))
+            goto failed;
+
+        new_elt = (luaconf_elt*)malloc(sizeof(luaconf_elt));
+        if (!new_elt)
+            goto failed;
+
+        lua_remove(L, -2);                  // remomve global table
+        new_elt->type = lua_type(L, -1);
+        new_elt->pos = lua_gettop(L);
+    }
+
+    new_elt->vname[0] = 0;
+    new_elt->L = L;
+
+    return new_elt;
+
+failed:
+
+    if (new_elt)
+        free(new_elt);
+
+    lua_settop(n);
+
+    return NULL;
 }
 
--- src/luaconf.h
+++ src/luaconf.h
@@ -2,10 +2,15 @@
 #ifndef _LUACONF_H
 #define _LUACONF_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
 
+#define LUACONF_ASSERT          assert
+#define LUACONF_MAX_NAME_LEN    256
 
 typedef struct luaconf_inst_s  luaconf_inst;
 typedef struct luaconf_elt_s   luaconf_elt;
@@ -13,10 +18,10 @@ typedef double luaconf_Number;
 
 
 typedef enum {
-    LUACONF_TYPE_NUMBER = 0,
-    LUACONF_TYPE_STRING,
-    LUACONF_TYPE_BOOL,
-    LUACONF_TYPE_TABLE
+    LUACONF_TYPE_NUMBER = LUA_TNUMBER,
+    LUACONF_TYPE_STRING = LUA_TSTRING,
+    LUACONF_TYPE_BOOL = LUA_TBOOLEAN,
+    LUACONF_TYPE_TABLE = LUA_TTABLE
 } luaconf_type;
 
 struct luaconf_inst_s {
@@ -26,7 +31,7 @@ struct luaconf_inst_s {
 
 struct luaconf_elt_s {
     lua_State       *L;
-    const char      *vname
+    char            vname[LUACONF_MAX_NAME_LEN];
     int             pos;
     luaconf_type    type;
 };
@@ -35,14 +40,17 @@ struct luaconf_elt_s {
 luaconf_inst *luaconf_init(const char *conf);
 void luaconf_destroy(luaconf_inst *inst);
 
-luaconf_elt * luaconf_getElts(luaconf_inst *inst, luaconf_elt *elt, const char *path, size_t *n);
+size_t luaconf_getSubEltCnt(luaconf_elt *elt);
+luaconf_elt ** luaconf_getSubElts(luaconf_elt *elt, luaconf_elt **vec, size_t size, size_t *n);
 
-luaconf_elt * luaconf_getElt(luaconf_inst *inst, luaconf_elt *elt, const char *path);
-void luaconf_freeElt(luaconf_inst *inst, luaconf_elt *elt);
+luaconf_elt * luaconf_getElt(luaconf_inst *inst, const char *path, size_t path_len);
+luaconf_elt * luaconf_getEltElt(luaconf_elt *elt, const char *path, size_t path_len);
 
-luaconf_Number luaconf_getNumber(luaconf_inst *inst, luaconf_elt *elt, const char *path);
-const char * luaconf_getString(luaconf_inst *inst, luaconf_elt *elt, const char *path, size_t *len);
-int luaconf_getBool(luaconf_inst *inst, luaconf_elt *elt, const char *path);
+void luaconf_freeElt(luaconf_elt *elt);
+
+luaconf_Number luaconf_getNumber(luaconf_elt *elt);
+char * luaconf_getStr(luaconf_elt *elt, char *buf, size_t size, size_t *len);
+size_t luaconf_getStrLen(luaconf_elt *elt);
 
 #endif
 
