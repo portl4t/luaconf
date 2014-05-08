4da42e602184349acf10021e998d001c307fb148
portl4t <portl4t.cn@gmail.com>
2014-05-08 17:16:12 +0800

    add deep.c

---
+++ t/deep.c
@@ -0,0 +1,45 @@
+
+/*
+
+   -- conf.lua
+
+   site1 = {
+       ['domain'] = 'www.taobao.com',
+       ['level'] = 6,
+       ['sub'] = {
+                    ['prefix'] = 'http',
+                    ['port'] = 7799
+                 }
+   }
+
+*/
+
+#include "luaconf.h"
+
+
+int main(int argc, char *argv[])
+{
+    long            n;
+    size_t          size;
+    luaconf_inst    *inst;
+    luaconf_elt     *elt1;
+    luaconf_elt     *elt2;
+    char            buf[1024];
+
+    inst = luaconf_init("conf.lua");
+
+    elt1 = luaconf_getElt(inst, "site1", sizeof("site1"));
+    n = lua_gettop(elt1->L);
+
+    elt2 = luaconf_getEltElt(elt1, "sub.prefix", sizeof("sub.prefix")-1);
+    n = lua_gettop(elt1->L);
+
+    luaconf_getStr(elt2, buf, sizeof(buf), &size);
+
+    luaconf_freeElt(elt1);
+    luaconf_freeElt(elt2);
+    luaconf_destroy(inst);
+
+    return 0;
+}
+
